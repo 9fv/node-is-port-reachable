e44652c052bf7d1962ed98e45c7c1891334bdfbe
François Michaud <fmichaud@9fevrier.com>
2018-02-11 17:49:41 +0100

    Fix bug on timeout

--- test/is-port-reachable.test.js
+++ test/is-port-reachable.test.js
@@ -11,19 +11,19 @@ const should = require('should');
 
 const {PortReachable} = require('../lib/is-port-reachable');
 
-describe('', () => {
-
-  it('', () => {
+describe('Test a valid port of valid host ', () => {
+  it('should return a {Promise}', () => {
     return (PortReachable.factory('www.google.com:80').try()).should.be.a.Promise;
   });
-
-  it('', () => {
+  it('should be finally equal to {true}', () => {
     return (PortReachable.factory('www.google.com:80').try()).should.be.finally.equal(true);
   });
-
-  it('', () => {
-    return (PortReachable.factory('www.google.com:444').try()).should.be.rejected();
-  }).timeout(5000);
-
-
 });
+
+
+describe('Test an invalid port of valid host ', () => {
+  it('should be rejected', () => {
+    return (PortReachable.factory('www.google.com:444').try()).should.be.rejected();
+  });
+});
+
